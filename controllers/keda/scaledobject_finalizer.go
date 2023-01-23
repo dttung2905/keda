2608b193743f4ec2846b30a69393580e7ca6cb9d
dttung2905 <ttdao.2015@accountancy.smu.edu.sg>
2023-01-23 13:17:52 +0800

    scaledobject_finalizer.go changes
    Signed-off-by: dttung2905 <ttdao.2015@accountancy.smu.edu.sg>

--- controllers/keda/scaledobject_finalizer.go
+++ controllers/keda/scaledobject_finalizer.go
@@ -20,9 +20,11 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	appsv1 "k8s.io/api/apps/v1"
+	autoscalingv1 "k8s.io/api/autoscaling/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 
 	kedav1alpha1 "github.com/kedacore/keda/v2/apis/keda/v1alpha1"
 	"github.com/kedacore/keda/v2/controllers/keda/util"
@@ -53,7 +55,9 @@ func (r *ScaledObjectReconciler) finalizeScaledObject(ctx context.Context, logge
 				logger.V(1).Info("Failed to restore scaleTarget's replica count back to the original, the scaling haven't been probably initialized yet.")
 			} else {
 				// We have enough information about the scaleTarget, let's proceed.
-				scale, err := r.ScaleClient.Scales(scaledObject.Namespace).Get(ctx, scaledObject.Status.ScaleTargetGVKR.GroupResource(), scaledObject.Spec.ScaleTargetRef.Name, metav1.GetOptions{})
+				scale := &autoscalingv1.Scale{}
+				dep := &appsv1.Deployment{}
+				err := r.Client.Get(ctx, client.ObjectKey{Name: scaledObject.Spec.ScaleTargetRef.Name, Namespace: scaledObject.Namespace}, dep)
 				if err != nil {
 					if errors.IsNotFound(err) {
 						logger.V(1).Info("Failed to get scaleTarget's scale status, because it was probably deleted", "error", err)
@@ -62,7 +66,7 @@ func (r *ScaledObjectReconciler) finalizeScaledObject(ctx context.Context, logge
 					}
 				} else {
 					scale.Spec.Replicas = *scaledObject.Status.OriginalReplicaCount
-					_, err = r.ScaleClient.Scales(scaledObject.Namespace).Update(ctx, scaledObject.Status.ScaleTargetGVKR.GroupResource(), scale, metav1.UpdateOptions{})
+					err = r.Client.Update(ctx, scaledObject)
 					if err != nil {
 						logger.Error(err, "Failed to restore scaleTarget's replica count back to the original", "finalizer", scaledObjectFinalizer)
 					}
